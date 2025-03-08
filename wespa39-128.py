fe566c49278654ab9af122ea2c63d35e47693637
mnstewart232 <mnstewart232@gmail.com>
2025-03-08 13:04:33 -0500

    Initial commit

---
+++ wespa39-128.py
@@ -0,0 +1,37 @@
+import math.py
+import serial
+from tkinter import *
+from tkinter import ttk
+
+def main():
+
+
+#Major parts of the program:
+#Inputs:
+    #Barcode scanner, connected via USB, keyboard-like input in Line39 or Line128 format.
+    # Line39 currently, simply the length
+    # Line128 may be a future expansion with the Work Order as the first 4 digits, length as remainder.
+    #Laser scanner, COM port, unknown format
+    #Config file, located in a sufficiently obscure place (like AppData)
+    #Laser offset value, tolerance, and other non-operator settings in Config file
+#Outputs:
+    #Zebra Printer, connected via USB, uses ZPL II
+#GUI:
+    #Display info from laser scanner and barcode, refresh laser in realtime:
+    #Table Length/Actual Length in Ft + Decimal Inches
+    #Order Length/Desired Length in Ft + Decimal Inches
+    #"Off by" Length being difference of the two
+    #Green/Yellow/Red Tolerance indicator
+    #Laser On/Off toggle buttons
+    #Clear button to refresh info from inputs (X button on keyboard)
+    #Print button toggled by spacebar; disable if measurements not within tolerance
+    
+    
+        
+#Libraries:
+    #Pyinstaller for compiling to a single exe
+    #tkinter for GUI
+    #pyserial for COM IO
+
+
+    return
