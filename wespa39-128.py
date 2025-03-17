5e0f5363370ca69831bb61c99b300f04a5f0c890
mnstewart232 <mnstewart232@gmail.com>
2025-03-16 22:29:39 -0400

    Barcode inputs working, all GUI fields refreshing appropriately. Put finishing touches on basic GUI, then figure out printing and laser scanner.

--- wespa39-128.py
+++ wespa39-128.py
@@ -1,7 +1,7 @@
 import math
 import serial
-from tkinter import *
-from tkinter import ttk
+import tkinter as ttk
+from tkinter import font
 
 #Major parts of the program:
 #Inputs:
@@ -39,55 +39,61 @@ def decInchesToFtIn(dec_inches):
         return "{0} FT {1} IN".format(feet, inches)
     else:   
         return "{0} IN".format(inches)
+    
+def resetLaser():
+    print("Resetting Laser...")
+
+
+def printLabel(isEnabled):
+    if isEnabled == "normal":
+        print("Printing Label...")
 
 
 class MainMenu:
-
-    def clear():
-        print("Clearing Barcodes...")
-
-
-    def resetLaser():
-        print("Resetting Laser...")
-
-
-    def printLabel():
-        print("Printing Label...")
-
-    scannerInput = ""
-    currentBarcode = ""
+    scannerInput = "" #Barcode scanner input
+    currentBarcode = "" #Last barcode scanned - delimited by newlines with the scanner
     orderVal = "" #First 4 digits of a line128 barcode
     orderLength = 0.0 #line39 code, or the remaining digits of a line128
-    tableLength = 284.79 #23ft 8.79in #Fill this in from laser scanner
-    offByVal = 0.0    
+    tableLength = 123.51 #Fill this in from laser scanner
+    offByVal = 0.0
     minTolerance = 0.1 #Fill this in from config file?
     maxTolerance = 6.0 #Fill this in from config file?
     allowPrint = "disabled"
-    toleranceIndicatorVal = ""
-    toleranceColorVal = ""
+    toleranceIndicatorVal = "Outside Tolerance"
+    toleranceColorVal = "red"
 
-    #Test values
-    #orderLength = 284.75 #23ft 8.75in
-    #orderVal = "1234"
+    lbl_workOrderVal = None
+    lbl_lengthVal = None
+    lbl_toleranceIndicator = None
+    lbl_tableLengthBox = None
+    lbl_offByBox = None
+    lbl_orderLengthBox = None
+    
+    btn_print = None
 
-    #Call this once the bardcode has been detected. Update the GUI with the new barcode.
+    def clear(self):
+        print("Clearing Barcodes...")
+        self.scannerInput = ""
+        self.currentBarcode = ""
+        self.updateGUI()
+        
+
+    #Call this once a bardcode has been detected and as the laser refreshes. Update the GUI with the new information.
     def updateGUI(self):
-        print("Updating GUI. Barcode: {self.currentBarcode}")
+        print(f"Updating GUI. Barcode: {self.currentBarcode}; Laser Length: {self.tableLength}")
 
         self.orderVal = ""
         self.orderLength = 0.0
         
         #Until Line128 is used, Work Order won't be in the barcode - be sure to code for it not being there.
-
         #This assumes a Line128 style code. How to better detect what kind of code it is?
-        #Simple, check if the first 4 chars are all digits. If so, it's a Line128 code.
+        #Simple, check if the first 4 chars are all digits (Line39 has 3 max). If so, it's a Line128 code.
         if (self.currentBarcode[0:4].isdigit()):
             self.orderVal = self.currentBarcode[0:4]
-            self.orderLength = float(self.currentBarcode[4:])
-        else:
-            #Otherwise it's line39!
-            self.orderLength = float(self.currentBarcode)
-
+            self.orderLength = float(self.currentBarcode[4:]).__round__(2)
+        elif(self.currentBarcode != ""):
+            #If we get here and the barcode isn't empty, it's probably a valid Line39 code.
+            self.orderLength = float(self.currentBarcode).__round__(2)
 
         self.offByVal = self.tableLength - self.orderLength
 
@@ -99,9 +105,27 @@ class MainMenu:
         elif abs(self.offByVal) <= self.maxTolerance and abs(self.offByVal) > self.minTolerance:
             self.toleranceIndicatorVal = "Near Tolerance"
             self.toleranceColorVal = "yellow"
+            self.allowPrint = "disabled"
         else:
             self.toleranceIndicatorVal = "Outside Tolerance"
             self.toleranceColorVal = "red"
+            self.allowPrint = "disabled"
+
+        self.lbl_workOrderVal.config(text=self.orderVal)
+        self.lbl_lengthVal.config(text=decInchesToFtIn(self.orderLength))
+        self.lbl_toleranceIndicator.config(text=self.toleranceIndicatorVal, background=self.toleranceColorVal)
+        self.btn_print.configure(state=self.allowPrint)
+        self.lbl_tableLengthBox.config(text=decInchesToFtIn(self.tableLength))
+        self.lbl_offByBox.config(text=decInchesToFtIn(self.offByVal))
+        self.lbl_orderLengthBox.config(text=decInchesToFtIn(self.orderLength))
+
+        self.lbl_workOrderVal.update()
+        self.lbl_lengthVal.update()
+        self.lbl_toleranceIndicator.update()
+        self.btn_print.update()
+        self.lbl_tableLengthBox.update()
+        self.lbl_offByBox.update()
+        self.lbl_orderLengthBox.update()
 
         print(f"Order Length: {self.orderLength}, Order Value: {self.orderVal}")
 
@@ -131,56 +155,68 @@ class MainMenu:
 
         # Bind keyboard shortcuts; also detect barcode input
         root.bind('<x>', lambda event: self.clear())
-        root.bind('<l>', lambda event: self.resetLaser())
-        root.bind('<space>', lambda event: self.printLabel())
+        root.bind('<l>', lambda event: resetLaser())
+        root.bind('<space>', lambda event: printLabel(self.allowPrint))
         root.bind('<Key>', self.captureInput)
         
-        #How to input?
-        #root.bind('<Key>', self.captureInput)
+        small_bold_font = font.Font(size=18, weight="bold")
+        medium_font = font.Font(size=24)
+        large_font = font.Font(size=36)
+        xl_font = font.Font(size=48)
         
-        lbl_lastBarcode = ttk.Label(content, text="Last Barcode Scanned:")
+        lbl_lastBarcode = ttk.Label(content, text="Last Barcode Scanned:", font=small_bold_font)
         lbl_lastBarcode.grid(column=0, row=0, padx=5, pady=5, sticky="W")
 
-        lbl_workOrder = ttk.Label(content, text="Work Order: ")
+        lbl_workOrder = ttk.Label(content, text="Work Order: ", font = medium_font)
         lbl_workOrder.grid(column=1, row=0, padx=5, pady=5, sticky="W")
 
-        lbl_workOrderVal = ttk.Label(content, text=self.orderVal)
-        lbl_workOrderVal.grid(column=2, row=0, padx=5, pady=5, sticky="W")
+        self.lbl_workOrderVal = ttk.Label(content, text=self.orderVal, font=medium_font)
+        self.lbl_workOrderVal.grid(column=2, row=0, padx=5, pady=5, sticky="W")
         
-        lbl_length = ttk.Label(content, text="Length: ")
+        lbl_length = ttk.Label(content, text="Length: ", font=medium_font)
         lbl_length.grid(column=1, row=1, padx=5, pady=5, sticky="W")
 
-        lbl_lengthVal = ttk.Label(content, text=decInchesToFtIn(self.orderLength))
-        lbl_lengthVal.grid(column=2, row=1, padx=5, pady=5, sticky="W")
+        self.lbl_lengthVal = ttk.Label(content, text=decInchesToFtIn(self.orderLength), font=medium_font)
+        self.lbl_lengthVal.grid(column=2, row=1, padx=5, pady=5, sticky="W")
 
-        btn_clear = ttk.Button(content, text="CLEAR (X)")
+        btn_clear = ttk.Button(content, text="CLEAR (X)", font=small_bold_font)
         btn_clear.grid(column=3, row=0, rowspan=2)
         btn_clear.bind('<Button-1>', lambda event: self.clear())
 
-        btn_resetLaser = ttk.Button(content, text="RESET LASER (L)")
+        btn_resetLaser = ttk.Button(content, text="RESET LASER (L)", font=small_bold_font)
         btn_resetLaser.grid(column=4, row=0, rowspan=2)
-        btn_resetLaser.bind('<Button-1>', lambda event: self.resetLaser())
+        btn_resetLaser.bind('<Button-1>', lambda event: resetLaser())
         
-        lbl_tableLength = ttk.Label(content, text="TABLE LENGTH: " + decInchesToFtIn(self.tableLength))
+        lbl_tableLength = ttk.Label(content, text="TABLE LENGTH:", justify="left", font=medium_font)
         lbl_tableLength.grid(column=0, row=2, columnspan=2, padx=5, pady=5, sticky="W")
 
-        lbl_offBy = ttk.Label(content, text="OFF BY: " + decInchesToFtIn(self.offByVal))
+        self.lbl_tableLengthBox = ttk.Label(content, text=decInchesToFtIn(self.tableLength), justify="left", font=large_font, background="white", relief="solid")
+        self.lbl_tableLengthBox.grid(column=0, row=3, columnspan=2, padx=5, pady=5, sticky="W")
+
+        #Could make 3 more labels for the relevant stats...
+
+        lbl_offBy = ttk.Label(content, text="OFF BY:", justify="left", font=medium_font)
         lbl_offBy.grid(column=2, row=2, columnspan=1, padx=5, pady=5, sticky="W")
 
-        lbl_orderLength = ttk.Label(content, text="ORDER LENGTH: " + decInchesToFtIn(self.orderLength))
-        lbl_orderLength.grid(column=3, row=2, columnspan=2, padx=5, pady=5, sticky="W")
+        self.lbl_offByBox = ttk.Label(content, text=decInchesToFtIn(self.offByVal), justify="left", font=xl_font, background="white", relief="solid")
+        self.lbl_offByBox.grid(column=2, row=3, columnspan=1, padx=5, pady=5, sticky="W")
 
+        lbl_orderLength = ttk.Label(content, text="ORDER LENGTH:", justify="left", font=medium_font)
+        lbl_orderLength.grid(column=3, row=2, columnspan=2, padx=5, pady=5, sticky="E")
 
-        lbl_toleranceIndicator = ttk.Label(content, text=self.toleranceIndicatorVal, background=self.toleranceColorVal)
-        lbl_toleranceIndicator.grid(column=1, row=4, columnspan=3, padx=5, pady=5, sticky="S")
+        self.lbl_orderLengthBox = ttk.Label(content, text=decInchesToFtIn(self.orderLength), justify="right", font=xl_font, background="white", relief="solid")
+        self.lbl_orderLengthBox.grid(column=3, row=3, columnspan=2, padx=5, pady=5, sticky="E")
+
+        self.lbl_toleranceIndicator = ttk.Label(content, text=self.toleranceIndicatorVal, background=self.toleranceColorVal, font=xl_font)
+        self.lbl_toleranceIndicator.grid(column=1, row=4, columnspan=3, padx=5, pady=5, sticky="S")
         
-        btn_print = ttk.Button(content, text="PRINT\n(spacebar)")
-        btn_print.configure(state=self.allowPrint)
-        btn_print.bind("<Button-1>", lambda event: self.printLabel())
-        btn_print.grid(column=2, row=5, rowspan=1, padx=5, pady=5, sticky="S")
+        self.btn_print = ttk.Button(content, text="PRINT\n(spacebar)", font=small_bold_font)
+        self.btn_print.grid(column=2, row=5, rowspan=1, padx=5, pady=5, sticky="S")
+        self.btn_print.bind("<Button-1>", lambda event: printLabel(self.allowPrint))
+        self.btn_print.configure(state=self.allowPrint)
+        
 
 
-root = Tk()
+root = ttk.Tk()
 MainMenu(root)
 root.mainloop()
-
